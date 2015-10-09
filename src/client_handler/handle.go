d8768dc286a57cc72551a5d48874d0ee30c72932
xtaci <daniel820313@gmail.com>
2015-10-09 12:46:19 +0800

    update proto_gen

--- src/client_handler/handle.go
+++ src/client_handler/handle.go
@@ -3,16 +3,7 @@ package client_handler
 import (
 	"misc/packet"
 	. "types"
-
-	log "github.com/gonet2/libs/nsq-logger"
 )
-
-func checkErr(err error) {
-	if err != nil {
-		log.Error(err)
-		panic("error occured in protocol module")
-	}
-}
 
 //----------------------------------- ping
 func P_proto_ping_req(sess *Session, reader *packet.Packet) []byte {
