333737c8b72267ba50d84642ad73a6cbbdf45193
xtaci <daniel820313@gmail.com>
2015-11-12 23:08:09 +0800

    optimize registry by no-defer for fast access

--- src/registry/registry.go
+++ src/registry/registry.go
@@ -24,29 +24,31 @@ func (r *Registry) init() {
 // register a user
 func (r *Registry) Register(id int32, v interface{}) {
 	r.Lock()
-	defer r.Unlock()
 	r.records[id] = v
+	r.Unlock()
 }
 
 // unregister a user
 func (r *Registry) Unregister(id int32) {
 	r.Lock()
-	defer r.Unlock()
 	delete(r.records, id)
+	r.Unlock()
 }
 
 // query a user
-func (r *Registry) Query(id int32) interface{} {
+func (r *Registry) Query(id int32) (x interface{}) {
 	r.RLock()
-	defer r.RUnlock()
-	return r.records[id]
+	x = r.records[id]
+	r.RUnlock()
+	return
 }
 
 // return count of online users
-func (r *Registry) Count() int {
+func (r *Registry) Count() (count int) {
 	r.RLock()
-	defer r.RUnlock()
-	return len(r.records)
+	count = len(r.records)
+	r.RUnlock()
+	return
 }
 
 func Register(id int32, v interface{}) {
